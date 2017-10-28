431ac3c0a9e29e9ba5f38c2aa14c71b1aefa459b
Francisco Dominguez Mateos <francisco.dominguez@urjc.es>
2017-10-28 18:24:28 +0200

    Init vistas

---
+++ cPAmotorJuegosV1.2_Vistas/src/escena.h
@@ -0,0 +1,45 @@
+/*
+ * escena.h
+ *
+ *  Created on: 18 Oct 2016
+ *      Author: francisco
+ */
+
+#ifndef ESCENA_H_
+#define ESCENA_H_
+#include <vector>
+#include "vector3d.h"
+#include "solido.h"
+using namespace std;
+
+class Escena {
+	vector<Solido*> solidos;
+public:
+	Escena();
+	Escena(const Escena &e){
+		for(Solido *s:e.solidos){
+			solidos.push_back(s->clone());
+		}
+	}
+	virtual ~Escena();
+	void render(){
+	 for(Solido *s:solidos)
+	  s->render();
+	}
+	void update(double dt){
+		 for(Solido *s:solidos){
+			 s->setF(Vector3D(0,-9.8/1,0));
+			 s->update(dt);
+		 }
+	}
+	vector<Solido*> getSolidos(){return solidos;}
+	void add(Solido *s){solidos.push_back(s);}
+	friend std::ostream &operator << (std::ostream &os, const Escena &v);
+};
+inline std::ostream &operator<<(std::ostream &os, const Escena &e){
+	for(Solido *s:e.solidos){
+		os << *s <<endl;
+	}
+    return os;
+}
+#endif /* ESCENA_H_ */
---
+++ cPAmotorJuegosV1.2_Vistas/src/solido.h
@@ -0,0 +1,71 @@
+/*
+ * solido.h
+ *
+ *  Created on: 25 Oct 2016
+ *      Author: francisco
+ */
+
+#ifndef SOLIDO_H_
+#define SOLIDO_H_
+#include <GL/glut.h>
+#include "vector3d.h"
+
+class Solido {
+	 Vector3D pos;
+	 Vector3D vel;
+	 Vector3D color;
+	 Vector3D f;
+	 double m;
+	 bool fija;
+public:
+	 Solido():pos(0,0,0),vel(0,0,0),color(1,1,1),f(0,0,0),m(1.0),fija(false) {}
+	 Solido(double x,double y,double z):pos(x,y,z),vel(0,0,0),color(1,1,0),f(0,0,0),m(1.0),fija(false){}
+	 Solido(const Solido &s):pos(s.pos),vel(s.vel),color(s.color),f(s.f),m(s.m),fija(false){}
+	 virtual Solido *clone (){return new Solido(*this);}
+	 inline Vector3D getPos(){return pos;}
+	 inline Vector3D getVel(){return vel;}
+	 inline Vector3D getCol(){return color;}
+	 inline Vector3D getF(){return f;}
+	 inline double getM(){return m;}
+	 inline bool getFija(){return fija;}
+	 inline void setPos(Vector3D p){pos=p;}
+	 inline void setVel(Vector3D v){vel=v;}
+	 inline void setCol(Vector3D c){color=c;}
+	 inline virtual void setF(Vector3D v){f=v;}
+	 inline void setM(double mp){m=mp;}
+	 inline void setFija(bool b){fija=b;}
+	 inline void hazFija (){fija=true;}
+	 inline void hazMovil(){fija=false;}
+	 inline void limpiaFuerza(){f=Vector3D();}
+	 inline void aplicaFuerza (Vector3D fv){f=f+fv;}
+	 inline void acumulaFuerza(Vector3D fv){f=f+fv;}
+	 inline virtual void update(double dt){
+		 if(fija)
+			 return;
+		 vel=vel+f/m*dt;
+		 pos=pos+vel*dt;
+		 if(abs(pos.getX())>50.0){
+			 vel.setX(vel.getX()*-0.9);
+		 }
+		 if(abs(pos.getZ())>50){
+		 	 vel.setZ(vel.getZ()*-0.9);
+		 }
+		 if(pos.getY()<0){
+			 vel.setY(vel.getY()*-0.9);
+			 pos.setY(0.0001);
+		 }
+	 }
+	 virtual void render(){
+		   glPushMatrix();
+		   glColor3f(this->getCol().getX(),this->getCol().getY(),this->getCol().getZ());
+		   glTranslatef(this->getPos().getX(),this->getPos().getY(),this->getPos().getZ());
+		   glutSolidSphere(0.1,10,10);
+		   glPopMatrix();
+	 }
+    friend std::ostream &operator << (std::ostream &os, const Solido &v);
+};
+inline std::ostream &operator<<(std::ostream &os, const Solido &s){
+    os << "Pos=" << s.pos << ",Vel=" << s.vel << ",Col=" << s.color;
+    return os;
+}
+#endif /* SOLIDO_H_ */
