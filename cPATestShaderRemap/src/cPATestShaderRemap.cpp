f641800aff5f1340f84932217639d1880746ce6d
Francisco Dominguez Mateos <francisco.dominguez@urjc.es>
2021-01-22 23:40:31 +0100

    remap work in stereo images

--- cPATestShaderRemap/src/cPATestShaderRemap.cpp
+++ cPATestShaderRemap/src/cPATestShaderRemap.cpp
@@ -74,7 +74,13 @@ Mat getRightMap(){
 	  cols: 4
 	  data: [953.0537522907438, 0, 384.4876556396484, -57.95554829812738, 0, 953.0537522907438, 276.9204998016357, 0, 0, 0, 1, 0]
 	 */
-
+	Mat K=(Mat_<double>(3,3)<<958.8710177402639, 0, 310.8323417297909, 0, 954.3393084573635, 282.4460052293588, 0, 0, 1);
+	Mat d=(Mat_<double>(1,5)<<-0.5177046435417091, 0.4190207743279942, -0.01204088578226774, -0.002396026919730114, 0);
+	Mat R=(Mat_<double>(3,3)<<0.9968596736669864, 0.008162426114800682, -0.07876652725917205, -0.007394536192554946, 0.9999222997971208, 0.01003569643492776, 0.07884232271468458, -0.009421739136563212, 0.9968425748234252);
+	Mat P=(Mat_<double>(3,4)<<953.0537522907438, 0, 384.4876556396484, 0/*-57.95554829812738*/, 0, 953.0537522907438, 276.9204998016357, 0, 0, 0, 1, 0);
+	Mat map1,map2;
+	initUndistortRectifyMap(K,d,R,P,Size(640,480),CV_32FC2,map1,map2);
+	return map1;
 }
 class TestShaderRemap:public GameStandard{
 	Texture texGround;
@@ -91,6 +97,11 @@ public:
 	virtual void keyPressed(char key,int x,int y){
 		GameStandard::keyPressed(key,x,y);
 		lookAtAxis->setPos(getCamera()->getLookAtPos());
+	}
+	Mat stackH(Mat im1,Mat im2){
+		Mat im3;
+		hconcat(im1,im2,im3);
+		return im3;
 	}
 	//TestImageFilters::init()
 	void init(){
@@ -128,12 +139,12 @@ public:
         Mat imgL=imgLo.clone();
         Mat imgR=imgRo.clone();
 
-        imshow("imgL",imgL);
+        //imshow("imgL",imgL);
         Mat mapL=getLeftMap();
 
         Mat imgLrect;
-        remap(imgL,imgLrect,mapL,Mat(),INTER_LINEAR);
-        imshow("imgLrect",imgLrect);
+        //remap(imgL,imgLrect,mapL,Mat(),INTER_LINEAR);
+        //imshow("imgLrect",imgLrect);
 
         Mat fboImg;
 	    ShaderBinOp remap(nullptr,640,480,"vec4 opt(vec4 v0,vec4 v1){return remap(v0,v1);}");
@@ -147,8 +158,28 @@ public:
 		ptex1->init();
 		remap.setTex1(ptex1);
 		remap.render();
-		fboImg=remap.downloadResult();
-		imshow("ShaderBinOp/remap",fboImg);
+		imgLrect=remap.downloadResult();
+
+        //imshow("imgR",imgR);
+        Mat mapR=getRightMap();
+
+        Mat imgRrect;
+	    ShaderBinOp remapR(nullptr,640,480,"vec4 opt(vec4 v0,vec4 v1){return remap(v0,v1);}");
+	    remapR.setImage(imgR);
+		//Mat m=Mat::zeros(480,640,CV_32FC3);
+		//circle(m,Point( 240, 320 ), 32*4.0, Scalar( 0, 0, 1 ), 10, 8 );
+		//imshow("mapL",m);
+		//TexturePtr ptex1=new Texture(m);
+		TexturePtr ptex1R=new Texture(mapR);
+		ptex1R->init();
+		remapR.setTex1(ptex1R);
+		remapR.render();
+		imgRrect=remapR.downloadResult();
+
+		fboImg=stackH(imgLrect,imgRrect);
+		for(int row=32;row<480  ;row+=32)line(fboImg,Point(0,row),Point(640*2,row),Scalar(255,0,0),1);
+		for(int col=32;col<640*2;col+=32)line(fboImg,Point(col,0),Point(col  ,480),Scalar(255,0,0),1);
+		imshow("ShaderBinOp/remapLR",fboImg);
 
 		ax=new Axis();
 		stage.add(ax);
